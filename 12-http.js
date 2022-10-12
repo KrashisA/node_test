3e3d75a41b5671bdf4f8be502ac1f4c67f2acd68
Krashis Adhikari <krashisadhikari@gmail.com>
2022-10-12 22:18:30 +0545

    Commiting the new http module learning

--- 12-http.js
+++ 12-http.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
     if (req.url === '') {
-        res.end('Welcome to our home page')
+        res.end('<h2>Welcome to our home page Bro!</h2>')
     }
     else if (req.url === '/') {
         res.end('Welcome to our home page BRO!')
